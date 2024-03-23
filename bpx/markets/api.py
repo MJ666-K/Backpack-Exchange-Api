7f5ce4c204a8c0626b7d3b83718d443f159aaed0
Mingjie <2212013739@qq.com>
2024-03-24 01:19:19 +0800

    markets接口全部完善

--- bpx/markets/api.py
+++ bpx/markets/api.py
@@ -9,10 +9,10 @@ class MarketsApi:
     def __init__(self, symbol: str = None) -> None:
         self.bp = Api()
         self.symbol = symbol
+        self.url = f"{self.bp.url}api/v1/"
 
     def get_assets(self):
-        url = f"{self.bp.url}api/v1/assets"
-        response = self.bp.get(url)
+        response = self.bp.get(self.url + "assets")
         if response:
             msg, code = response
             log.info(f"GetAssets: {code} {msg}")
@@ -21,5 +21,51 @@ class MarketsApi:
             log.info(f"GetAssets Error...")
 
     def get_markets(self):
-        # https://api.backpack.exchange/api/v1/markets
-        pass
+        response = self.bp.get(self.url + "markets")
+        if response:
+            msg, code = response
+            log.info(f"GetMarkets: {code} {msg}")
+            return msg
+        else:
+            log.info(f"GetMarkets Error...")
+
+    def get_ticker(self):
+        response = self.bp.get(self.url + "ticker", parameters={"symbol": self.symbol})
+        if response:
+            msg, code = response
+            log.info(f"GetTicker: {code} {msg}")
+            return msg
+        else:
+            log.info(f"GetTicker Error...")
+
+    def get_tickers(self):
+        response = self.bp.get(self.url + "tickers")
+        if response:
+            msg, code = response
+            log.info(f"GetTickers: {code} {msg}")
+            return msg
+        else:
+            log.info(f"GetTickers Error...")
+
+    def get_depth(self):
+        response = self.bp.get(self.url + "depth", parameters={"symbol": self.symbol})
+        if response:
+            msg, code = response
+            log.info(f"GetDepth: {code} {msg}")
+            return msg
+        else:
+            log.info(f"GetDepth Error...")
+
+    def get_klines(self, interval: str, startTime: int = None, endTime: int = None):
+        parameters = {"symbol": self.symbol, "interval": interval}
+        if not startTime:
+            parameters["startTime"] = startTime
+        if not endTime:
+            parameters["endTime"] = startTime
+        response = self.bp.get(self.url + "klines", parameters=parameters)
+        if response:
+            msg, code = response
+            log.info(f"GetKlines: {code} {msg}")
+            return msg
+        else:
+            log.info(f"GetKlines Error...")
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from bpx.markets.api import MarketsApi
 
 if __name__ == "__main__":
-    markets = MarketsApi()
-    markets.get_assets()
+    markets = MarketsApi("SOL_USDC")
+    # markets.get_assets()
+    # markets.get_ticker()
+    # markets.get_tickers()
+    # markets.get_markets()
+    # markets.get_depth()
+    # markets.get_klines("1m")
